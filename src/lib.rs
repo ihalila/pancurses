dfbcf345ff37f8edb017f21246683b43bfe68003
Ilkka Halila <ilkka@hali.la>
2016-03-13 17:02:39 +0200

    Started work on an idea for a better curses API.
    I figured a better api would be one where the methods
    that require a *mut WINDOW are actually methods on
    a CursesWindow struct. I think this makes more sense than
    the c-way of doing things.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,49 @@
+#[cfg(windows)]
 extern crate pdcurses;
+#[cfg(unix)]
+extern crate ncurses;
 
-use pdcurses::*;
+use pdcurses as curses;
+
+#[derive(Copy, Clone, Debug)]
+pub struct CursesWindow {
+	_window: *mut curses::WINDOW
+}
+
+impl CursesWindow {
+	///Return the current x coordinate of the cursor
+	pub fn cursor_x(&self) -> i32 {
+		unsafe { (*self._window)._curx }
+	}
+	
+	pub fn set_nodelay(&self, enabled: bool) {
+		unsafe { curses::nodelay(self._window, if enabled { 1u8 } else { 0u8 }); }
+	}
+}
 
 /// Initialize the curses system, this must be the first function that is called
 ///
 /// ```
-/// # use pancurses::initialize;
+/// # use pancurses::*;
 /// initialize();
+/// # end();
 /// ```
-pub fn initialize() -> *mut WINDOW {
-    unsafe { initscr() }
+pub fn initialize() -> CursesWindow {
+    let window_pointer = unsafe { curses::initscr() };
+    CursesWindow { _window : window_pointer }
+}
+
+pub fn end() {
+	unsafe { curses::endwin(); }
+}
+
+pub fn noecho() {
+	unsafe { curses::noecho(); }
+}
+
+#[test]
+pub fn test_cursor_position() {
+	let window = initialize();
+	assert!(window.cursor_x() == 0);
+	end();
 }
