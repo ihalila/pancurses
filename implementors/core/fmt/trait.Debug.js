b6c7914dd58a171178f5ae85ed304af1548f56e5
Ilkka Halila <ilkka@hali.la>
2016-03-19 17:26:22 +0200

    Initial commit of all docs for 0.1.0

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['pancurses'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='pancurses/struct.Window.html' title='pancurses::Window'>Window</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
