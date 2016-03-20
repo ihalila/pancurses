b933b4b0ab5392595e9eb494ea4938f695ec1432
Ilkka Halila <ilkka@hali.la>
2016-03-20 08:30:48 +0200

    Improved documentation

--- src/lib.rs
+++ src/lib.rs
@@ -43,22 +43,22 @@ pub struct Window {
 }
 
 impl Window {
-    /// attrset() sets the current attributes of the given window to attributes.
+    /// Sets the current attributes of the given window to attributes.
     pub fn attrset(&self, attributes: chtype) -> i32 {
         unsafe { curses::wattrset(self._window, attributes) }
     }
 
-    /// erase() copies blanks (i.e. the background chtype) to every cell of the window.
+    /// Copies blanks (i.e. the background chtype) to every cell of the window.
     pub fn erase(&self) -> i32 {
         unsafe { curses::werase(self._window) }
     }
 
-    /// With the getch(), wgetch(), mvgetch(), and mvwgetch() functions, a character is read from
-    /// the terminal associated with the window. In nodelay mode, if there is no input waiting, the
-    /// value ERR is returned. In delay mode, the program will hang until the system  passes text
-    /// through to the program. Depending on the setting of cbreak(), this will be after one
-    /// character or after the first newline.  Unless noecho() has been set, the character will
-    /// also be echoed into the designated window.
+    /// Read a character from the terminal associated with the window.
+    ///
+    /// In nodelay mode, if there is no input waiting, the value ERR is returned. In delay mode,
+    /// the program will hang until the system  passes text through to the program. Depending on
+    /// the setting of cbreak(), this will be after one character or after the first newline.
+    /// Unless noecho() has been set, the character will also be echoed into the designated window.
     ///
     /// If keypad() is TRUE, and a function key is pressed, the token for that function key will be
     /// returned instead of the raw characters.
@@ -78,36 +78,38 @@ impl Window {
         unsafe { curses::getmaxy(self._window) }
     }
 
-    /// The cursor associated with the window is moved to the given location.  This does not move
-    /// the physical cursor of the terminal until refresh() is called.  The position specified is
-    /// relative to the upper left corner of the window, which is (0,0).
+    /// The cursor associated with the window is moved to the given location.
+    ///
+    /// This does not move the physical cursor of the terminal until refresh() is called.  The
+    /// position specified is relative to the upper left corner of the window, which is (0,0).
     pub fn mv(&self, y: i32, x: i32) -> i32 {
         unsafe { curses::wmove(self._window, y, x) }
     }
 
-    /// Write all the characters of the string str  to the given window. The functionality is
+    /// Write all the characters of the string str to the given window. The functionality is
     /// similar to calling waddch() once for each character in the string.
     pub fn mvaddstr(&self, y: i32, x: i32, string: &str) -> i32 {
         let s = CString::new(string).unwrap();
         unsafe { curses::mvwaddstr(self._window, y, x, s.as_ptr()) }
     }
 
-    ///The printw() functions add a string to the window at the current cursor position.
+    ///Add a string to the window at the current cursor position.
     pub fn printw(&self, string: &str) -> i32 {
         let s = CString::new(string).unwrap();
         unsafe { curses::wprintw(self._window, s.as_ptr()) }
     }
 
-    /// refresh() copies the named window to the physical terminal screen, taking into account what
-    /// is already there in order to optimize cursor movement. These routines must be called to get
-    /// any output on the terminal, as other routines only manipulate data structures. Unless
-    /// leaveok() has been enabled, the physical cursor of the terminal is left at the location of
-    /// the window's cursor.
+    /// Copies the named window to the physical terminal screen, taking into account what
+    /// is already there in order to optimize cursor movement.
+    ///
+    /// This function must be called to get any output on the terminal, as other routines only
+    /// manipulate data structures. Unless leaveok() has been enabled, the physical cursor of the
+    /// terminal is left at the location of the window's cursor.
     pub fn refresh(&self) -> i32 {
         unsafe { curses::wrefresh(self._window) }
     }
 
-    /// nodelay() controls whether wgetch() is a non-blocking call. If the option is enabled, and
+    /// Controls whether wgetch() is a non-blocking call. If the option is enabled, and
     /// no input is ready, wgetch() will return ERR. If disabled, wgetch() will hang until input is
     /// ready.
     pub fn set_nodelay(&self, enabled: bool) -> i32 {
@@ -116,48 +118,53 @@ impl Window {
 }
 
 
-/// endwin() should be called before exiting or escaping from curses mode temporarily.  It will
-/// restore tty modes, move the cursor to the lower left corner of the screen and reset the
+/// Should be called before exiting or escaping from curses mode temporarily.
+///
+/// It will restore tty modes, move the cursor to the lower left corner of the screen and reset the
 /// terminal into the proper non-visual mode.  To resume curses after a temporary escape, call
 /// refresh() or doupdate().
 pub fn endwin() -> i32 {
     unsafe { curses::endwin() }
 }
 
-/// has_colors() indicates if the terminal supports, and can maniplulate color.
+/// Indicates if the terminal supports, and can maniplulate color.
 pub fn has_colors() -> bool {
     unsafe { curses::has_colors() > 0 }
 }
 
 /// Initialize the curses system, this must be the first function that is called.
+///
 /// Returns a Window struct that is used to access Window specific functions.
 pub fn initscr() -> Window {
     let window_pointer = unsafe { curses::initscr() };
     Window { _window: window_pointer }
 }
 
-/// init_pair() changes the definition of a color-pair. It takes three arguments: the number of the
-/// color-pair to be redefined, and the new values of the foreground and background colors. The
-/// pair number must be between 0 and COLOR_PAIRS - 1, inclusive. The foreground and background
-/// must be between 0 and COLORS - 1, inclusive. If the color pair was previously initialized, the
-/// screen is refreshed, and all occurrences of that color-pair are changed to the new definition.
+/// Changes the definition of a color-pair.
+///
+/// It takes three arguments: the number of the color-pair to be redefined, and the new values of
+/// the foreground and background colors. The pair number must be between 0 and COLOR_PAIRS - 1,
+/// inclusive. The foreground and background must be between 0 and COLORS - 1, inclusive. If the
+/// color pair was previously initialized, the screen is refreshed, and all occurrences of that
+/// color-pair are changed to the new definition.
 pub fn init_pair(pair_index: i16, foreground_color: i16, background_color: i16) -> i32 {
     unsafe { curses::init_pair(pair_index, foreground_color, background_color) as i32 }
 }
 
-/// napms() suspends the program for the specified number of milliseconds.
+/// Suspends the program for the specified number of milliseconds.
 pub fn napms(ms: i32) -> i32 {
     unsafe { curses::napms(ms) }
 }
 
-/// echo() and noecho() control whether typed characters are echoed by the input routine.
+/// Disables echoing typed characters.
+///
 /// Initially, input characters are echoed. Subsequent calls to echo() and noecho() do not flush
 /// type-ahead.
 pub fn noecho() -> i32 {
     unsafe { curses::noecho() }
 }
 
-/// start_color() initializes eight basic colors (black, red, green, yellow, blue, magenta, cyan,
+/// Initializes eight basic colors (black, red, green, yellow, blue, magenta, cyan,
 /// and white), and two global variables; COLORS and COLOR_PAIRS (respectively defining the
 /// maximum number of colors and color-pairs the terminal is capable of displaying).
 pub fn start_color() -> i32 {
