6bceb3937213c710004bd31ff77f0226679b8540
Ilkka Halila <ilkka@hali.la>
2016-07-31 20:19:18 +0300

    Implemented newtest up to but not including mouse handling.

--- examples/newtest.rs
+++ examples/newtest.rs
@@ -34,12 +34,25 @@ fn main() {
     const COL3: i32 = 72;
 
     let mut unicode_offset = 0x80;
+    let mut blink_state = true;
 
     while !quit {
         let (y_max, x_max) = window.get_max_yx();
         let color_block_start = 54;
         let mut color_block_cols = (x_max - color_block_start) / 2;
         let color_block_lines = 19;
+
+        let mut cursor_state_1 = 2;
+        let mut cursor_state_2 = 3;
+        let cursor_state_text = ["Invisible (click to change) ",
+                                 "Underscore (click to change)",
+                                 "Block (click to change)     ",
+                                 "Outline (click to change)   ",
+                                 "Caret (click to change)     ",
+                                 "Half-block (click to change)",
+                                 "Central (click to change)   ",
+                                 "Cross (click to change)     ",
+                                 "Heavy box (click to change) "];
 
         if color_block_cols < 0 {
             color_block_cols = 0;
@@ -87,12 +100,15 @@ fn main() {
             }
             window.mvprintw(13, 40, &format!("U+{:04X} ", unicode_offset));
 
-            for i in 0..128 { // Show extended characters
-                window.mvaddstr(5 + i % 16, (i / 16) * 5, &format!("{:02X} ", i + unicode_offset));
+            for i in 0..128 {
+                // Show extended characters
+                window.mvaddstr(5 + i % 16,
+                                (i / 16) * 5,
+                                &format!("{:02X} ", i + unicode_offset));
                 if i + unicode_offset > ' ' as i32 {
-                   window.addch((i + unicode_offset) as chtype);
+                    window.addch((i + unicode_offset) as chtype);
                 } else {
-                   window.addch(' ');
+                    window.addch(' ');
                 }
                 window.addch(' ');
             }
@@ -101,7 +117,7 @@ fn main() {
 
             redraw = false;
             window.attrset(COLOR_PAIR(1));
-            
+
             for i in 0..6 {
                 let line = 24 + i / 3;
                 let col = 5 + 25 * (i % 3);
@@ -111,7 +127,7 @@ fn main() {
                 let greek = "Ελληνικά";
                 let georgian = "ქართული ენა";
                 let fullwidth = " Ｆｕｌｌｗｉｄｔｈ";
-                let combining_marks =  "Cmin 	r";
+                let combining_marks = "Cmin 	r";
 
                 let texts = [spanish, russian, greek, georgian, fullwidth, combining_marks];
 
@@ -120,18 +136,57 @@ fn main() {
                 }
             }
 
-            window.mvaddstr(  1, COL3, "Click on cursor descriptions to");
-            window.mvaddstr(  2, COL3, "cycle through possible cursors");
-            window.mvaddstr(  3, COL3, "Click on colors at left to change");
-            window.mvaddstr(  4, COL3, "colors used for under/over/outlining");
-            window.mvaddstr(  5, COL3, "Click 'Blink' at bottom to toggle");
-            window.mvaddstr(  6, COL3, "'real' blinking vs. 'highlit' blink");
-
+            window.mvaddstr(1, COL3, "Click on cursor descriptions to");
+            window.mvaddstr(2, COL3, "cycle through possible cursors");
+            window.mvaddstr(3, COL3, "Click on colors at left to change");
+            window.mvaddstr(4, COL3, "colors used for under/over/outlining");
+            window.mvaddstr(5, COL3, "Click 'Blink' at bottom to toggle");
+            window.mvaddstr(6, COL3, "'real' blinking vs. 'highlit' blink");
         }
 
-        // These two lines are just here to allow running this until its finished
-        window.getch();
-        break;
+        window.mvaddnstr(19,
+                         color_block_start,
+                         cursor_state_text[cursor_state_1],
+                         x_max - color_block_start);
+        window.mvaddnstr(20,
+                         color_block_start,
+                         cursor_state_text[cursor_state_2],
+                         x_max - color_block_start);
+
+        for i in 0..color_block_cols * color_block_lines {
+            let n_color_blocks = 256;
+
+            window.attrset(COLOR_PAIR(if i >= n_color_blocks { 2 } else { i as chtype }));
+            if i > 2 && i < n_color_blocks {
+                init_pair(i as i16, i as i16, COLOR_BLACK);
+            }
+            if i % color_block_cols == 0 {
+                window.mv(i / color_block_cols, color_block_start);
+            }
+            window.attron(A_REVERSE);
+            window.addstr(" ");
+        }
+
+        window.mv(19, color_block_start - 3);
+        window.refresh();
+        let c = window.getch();
+        window.attrset(COLOR_PAIR(1));
+
+        match c {
+            Some(Input::KeyF1) => quit = true,
+            Some(Input::Character(x)) if x == 27 as char => quit = true,
+            Some(Input::KeyF2) => {
+                blink_state = !blink_state;
+                set_blink(blink_state);
+            },
+            Some(x) if x != Input::KeyMouse => {
+                window.mvaddstr(0, COL1, &format!("Key {:?} hit          ", x));
+            },
+            Some(Input::KeyMouse) => {
+                
+            }
+            _ => (),
+        }
     }
 
     endwin();
