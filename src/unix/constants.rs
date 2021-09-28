b4630d00c5300093e076b9f3836a7f8b7a5b9fae
Ilkka Halila <ilkka@hali.la>
2021-09-28 21:25:03 +0300

    Re-export constants from ncurses

--- src/unix/constants.rs
+++ src/unix/constants.rs
@@ -1,16 +1,16 @@
 use crate::input::Input;
 use ncurses;
 use ncurses::ll::{attr_t, chtype, mmask_t};
-use ncurses::NCURSES_ATTR_SHIFT;
 
-pub use ncurses::{ACS_LLCORNER, ACS_LRCORNER, ACS_ULCORNER, ACS_URCORNER};
-pub use ncurses::{ACS_BTEE, ACS_HLINE, ACS_LTEE, ACS_PLUS, ACS_RTEE, ACS_TTEE, ACS_VLINE};
-pub use ncurses::{ACS_S1, ACS_S9, ACS_BULLET, ACS_CKBOARD, ACS_DEGREE, ACS_DIAMOND, ACS_PLMINUS};
-pub use ncurses::{ACS_BLOCK, ACS_BOARD, ACS_DARROW, ACS_LANTERN, ACS_LARROW, ACS_RARROW,
-                  ACS_UARROW};
-pub use ncurses::{ACS_S3, ACS_S7, ACS_GEQUAL, ACS_LEQUAL, ACS_NEQUAL, ACS_PI, ACS_STERLING};
 pub use ncurses::{ACS_BBSS, ACS_BSSB, ACS_SBBS, ACS_SBSS, ACS_SSBB, ACS_SSBS, ACS_SSSB};
+pub use ncurses::{
+    ACS_BLOCK, ACS_BOARD, ACS_DARROW, ACS_LANTERN, ACS_LARROW, ACS_RARROW, ACS_UARROW,
+};
 pub use ncurses::{ACS_BSBS, ACS_BSSS, ACS_SBSB, ACS_SSSS};
+pub use ncurses::{ACS_BTEE, ACS_HLINE, ACS_LTEE, ACS_PLUS, ACS_RTEE, ACS_TTEE, ACS_VLINE};
+pub use ncurses::{ACS_BULLET, ACS_CKBOARD, ACS_DEGREE, ACS_DIAMOND, ACS_PLMINUS, ACS_S1, ACS_S9};
+pub use ncurses::{ACS_GEQUAL, ACS_LEQUAL, ACS_NEQUAL, ACS_PI, ACS_S3, ACS_S7, ACS_STERLING};
+pub use ncurses::{ACS_LLCORNER, ACS_LRCORNER, ACS_ULCORNER, ACS_URCORNER};
 
 // We need to re-export the BUTTONX_Y constants as 'mmask_t's, otherwise you can't compare them
 // to a MEVENT's bstate without casting
@@ -49,46 +49,41 @@ pub const BUTTON_SHIFT: mmask_t = ncurses::BUTTON_SHIFT as mmask_t;
 pub const BUTTON_CTRL: mmask_t = ncurses::BUTTON_CTRL as mmask_t;
 pub const BUTTON_ALT: mmask_t = ncurses::BUTTON_ALT as mmask_t;
 
-fn NCURSES_BITS(mask: u32, shift: u32) -> u32 {
-    mask << (shift + NCURSES_ATTR_SHIFT) as usize
-}
-
 pub fn COLOR_PAIR(n: chtype) -> attr_t {
-    NCURSES_BITS(n as u32, 0u32) as attr_t
+    ncurses::COLOR_PAIR(n as i16)
 }
 
-pub const COLOR_BLACK: i16 = 0;
-pub const COLOR_RED: i16 = 1;
-pub const COLOR_GREEN: i16 = 2;
-pub const COLOR_YELLOW: i16 = 3;
-pub const COLOR_BLUE: i16 = 4;
-pub const COLOR_MAGENTA: i16 = 5;
-pub const COLOR_CYAN: i16 = 6;
-pub const COLOR_WHITE: i16 = 7;
+pub use ncurses::COLOR_BLACK;
+pub use ncurses::COLOR_BLUE;
+pub use ncurses::COLOR_CYAN;
+pub use ncurses::COLOR_GREEN;
+pub use ncurses::COLOR_MAGENTA;
+pub use ncurses::COLOR_RED;
+pub use ncurses::COLOR_WHITE;
+pub use ncurses::COLOR_YELLOW;
 
-pub const A_ALTCHARSET: attr_t = (1u32 << (14u32 + NCURSES_ATTR_SHIFT)) as attr_t;
-pub const A_ATTRIBUTES: attr_t = (!0u32 << NCURSES_ATTR_SHIFT) as attr_t;
-pub const A_BLINK: attr_t = (1u32 << (11u32 + NCURSES_ATTR_SHIFT)) as attr_t;
-pub const A_BOLD: attr_t = (1u32 << (13u32 + NCURSES_ATTR_SHIFT)) as attr_t;
-pub const A_CHARTEXT: attr_t = (1u32 << NCURSES_ATTR_SHIFT) as attr_t;
-pub const A_COLOR: attr_t = ((((1u32) << 8) - 1u32) << NCURSES_ATTR_SHIFT) as attr_t;
-pub const A_DIM: attr_t = (1u32 << (12u32 + NCURSES_ATTR_SHIFT)) as attr_t;
-pub const A_ITALIC: attr_t = (1u32 << (23 + NCURSES_ATTR_SHIFT)) as attr_t;
-pub const A_INVIS: attr_t = (1u32 << (15u32 + NCURSES_ATTR_SHIFT)) as attr_t;
+pub const A_ALTCHARSET: attr_t = ncurses::A_ALTCHARSET();
+pub const A_ATTRIBUTES: attr_t = ncurses::A_ATTRIBUTES();
+pub const A_BLINK: attr_t = ncurses::A_BLINK();
+pub const A_BOLD: attr_t = ncurses::A_BOLD();
+pub const A_CHARTEXT: attr_t = ncurses::A_CHARTEXT();
+pub const A_COLOR: attr_t = ncurses::A_COLOR();
+pub const A_DIM: attr_t = ncurses::A_DIM();
+pub const A_ITALIC: attr_t = ncurses::A_ITALIC();
+pub const A_INVIS: attr_t = ncurses::A_INVIS();
 pub const A_LEFTLINE: attr_t = 0; // Not supported on ncurses
-pub const A_NORMAL: attr_t = 0;
+pub const A_NORMAL: attr_t = ncurses::A_NORMAL();
 pub const A_OVERLINE: attr_t = 0; // Not supported on ncurses
-pub const A_REVERSE: attr_t = (1u32 << (10 + NCURSES_ATTR_SHIFT)) as attr_t;
+pub const A_REVERSE: attr_t = ncurses::A_REVERSE();
 pub const A_RIGHTLINE: attr_t = 0; // Not supported on ncurses
-pub const A_STANDOUT: attr_t = (1u32 << (8 + NCURSES_ATTR_SHIFT)) as attr_t;
+pub const A_STANDOUT: attr_t = ncurses::A_STANDOUT();
 pub const A_STRIKEOUT: attr_t = 0; // Not supported on ncurses
-pub const A_UNDERLINE: attr_t = (1u32 << (9 + NCURSES_ATTR_SHIFT)) as attr_t;
-
-pub const KEY_RESIZE: i32 = 0o632;
+pub const A_UNDERLINE: attr_t = ncurses::A_UNDERLINE();
 
 pub const KEY_OFFSET: i32 = 0o0400;
-pub const KEY_F15: i32 = KEY_OFFSET + 0x17;
-pub const KEY_EVENT: i32 = KEY_OFFSET + 0o633;
+pub const KEY_RESIZE: i32 = ncurses::KEY_RESIZE;
+pub const KEY_F15: i32 = ncurses::KEY_F15;
+pub const KEY_EVENT: i32 = ncurses::KEY_EVENT;
 
 pub const SPECIAL_KEY_CODES: [Input; 108] = [
     Input::KeyCodeYes,
